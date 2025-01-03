9fd3101c4e295f3528c87c532422d26fa5f2e772
Dan Gunter <dkgunter@lbl.gov>
2025-01-03 11:27:25 -0800

    added version

---
+++ idaes_connectivity/version.py
@@ -0,0 +1,14 @@
+###############################################################################
+# PrOMMiS was produced under the DOE Process Optimization and Modeling
+# for Minerals Sustainability (“PrOMMiS”) initiative, and is
+# Copyright © 2024-2025 by the software owners: The Regents of the
+# University of California, through Lawrence Berkeley National
+# Laboratory, National Technology & Engineering Solutions of Sandia, LLC,
+# Carnegie Mellon University, West Virginia University Research
+# Corporation, University of Notre Dame, and Georgia Institute of
+# Technology. All rights reserved.
+###############################################################################
+"""
+Version
+"""
+VERSION = "0.0.1"
