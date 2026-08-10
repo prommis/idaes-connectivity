182eccafdc049c1d108b8dac5749467a4526d051
Dan Gunter <dkgunter@lbl.gov>
2026-08-10 11:45:28 -0700

    fixed broken test, needed to build model

--- idaes_connectivity/tests/test_base.py
+++ idaes_connectivity/tests/test_base.py
@@ -188,16 +188,19 @@ def test_unit_and_stream_values_formatter(klass):
 def test_mermaid_images(tmp_path):
     if sys.platform == "win32":
         pytest.skip("Skipping test_mermaid_images on Windows")
+        # it actually runs, but seems unwilling to kill the server process,
+        # so it hangs the test suite.  Not sure why.
 
+    model = example_flowsheet.build()
     conn = Connectivity(input_model=model.fs)
     mmd = Mermaid(conn, component_images=True, component_image_dir=tmp_path)
 
     images = 0
     for line in mmd.write(None).split("\n"):
-        # print(f"@@ check line='{line}' for image")
+        print(f"test_mermaid_images: check line='{line}' for image")
         m = re.match(r"\s*\w+@\s*\{\s*img:.*\}", line)
         if m:
             images += 1
-    assert images == 6
+    assert images == 3
 
     mmd.stop_image_server()
